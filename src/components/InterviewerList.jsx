8f2efcb71130036653653d778900f31d048857b4
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-05 15:25:27 -0600

    added interviewers list and refactored it

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "components/InterviewerList.scss";
-const InterviewerList = () => {
+import InterviewerListItem from "./InterviewerListItem";
+
+const InterviewerList = (props) => {
+  const interviewers = props.interviewers.map((i) => {
+    return (
+      <InterviewerListItem
+        key={i.id}
+        name={i.name}
+        avatar={i.avatar}
+        selected={i.id === props.interviewer}
+        setInterviewer={() => props.setInterviewer(i.id)}
+      />
+    );
+  });
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{interviewers}</ul>
     </section>
   );
 };
--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -10,10 +10,7 @@ const InterviewerListItem = (props) => {
   //
   return (
     <>
-      <li
-        className={interviewrsItemClass}
-        onClick={() => props.setInterviewer(props.id)}
-      >
+      <li className={interviewrsItemClass} onClick={props.setInterviewer}>
         <img
           className="interviewers__item-image"
           src={props.avatar}
