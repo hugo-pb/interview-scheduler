c26300398fcd88144294086001d6c1e6bcdc50a3
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-07 17:37:53 -0600

    refactored test and files structure

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,23 +1,20 @@
 export function getAppointmentsForDay(state, nameDay) {
-  const filteredNames = state.days.filter((day) => {
-    return day.name === nameDay;
-  });
-  if (!filteredNames.length) {
+  const foundDay = state.days.find((day) => day.name === nameDay);
+  if (!foundDay) {
     return [];
   }
-
-  const arr = filteredNames[0].appointments;
-
-  let arr2 = Object.keys(state.appointments);
-  arr2 = arr2.map((a) => Number(a));
-  arr2 = arr2.filter((a) => {
-    return arr.includes(a);
-  });
-
-  arr2 = arr2.map((a) => {
-    return state.appointments[a];
-  });
-  return arr, arr2;
+  return foundDay.appointments.map(
+    (appointmentID) => state.appointments[appointmentID]
+  );
 }
 
-export function getInterview(state, interview) {}
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+
+  return {
+    ...interview,
+    interviewer: state.interviewers[interview.interviewer],
+  };
+}
