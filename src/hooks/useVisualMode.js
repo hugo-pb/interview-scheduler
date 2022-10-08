b30cdf063ebb15c2b960517eaf8bfe2667f79ee5
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-07 23:19:00 -0600

    custom hooks to navegate from mode of appointment

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,6 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export default function UseVisualMode(initial) {
-  const [mode, setmode] = useState(initial);
-  return { mode };
-}
+const UseVisualMode = (initial) => {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (second) => {
+    setMode(second);
+    setHistory((prev) => [...prev, second]);
+  };
+
+  const back = () => {
+    if (history.length !== 1) {
+      let newmode = history.at(-2);
+      console.log(newmode);
+      setMode(newmode);
+      let newHistory = [...history];
+      newHistory.pop();
+      console.log("new;", newHistory);
+      setHistory(newHistory);
+    }
+  };
+
+  return { mode, transition, back };
+};
+
+export default UseVisualMode;
